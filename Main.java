ca1a22fa873217c4b1408b0a68ca4aab245ff9ab
dimon <dimon20.07@mail.ru>
2023-10-18 02:24:39 +0300

    .\Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("select" + "\n1 create an entry" + "\n2 open the entry");
+        Menu.m();
+    }
+}
