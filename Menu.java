67a6c28e63e20af38f012d8e0a041764ba7c1806
dimon <dimon20.07@mail.ru>
2023-10-18 21:00:28 +0300

    .\Menu.java

--- Menu.java
+++ Menu.java
@@ -1,36 +1,16 @@
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.Scanner;
 
-public class Menu extends Main{
-    public static void m() throws IOException {
+public class Menu extends Main {
+    public static void menu() throws Exception {
         Scanner scan = new Scanner(System.in);
          int x = scan.nextInt();
 
         switch (x) {
             case 1:
-                System.out.println("enter the name");
-                FileWriter writer = new FileWriter("res.txt");
-                Scanner wow = new Scanner(System.in);
-                String a = wow.nextLine();
-                writer.append(a);
-                System.out.println("entry create");
-                writer.close();
-                break;
+                Writer.writer();
             case 2:
-                String text = "";
-                try {
-                    Reader reader = new FileReader("res.txt");
-                    while (reader.ready()) {
-                        text += (char) reader.read();
-                    }
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-                System.out.println(text);
-                break;
+                Read.read();
         }
     }
 }
